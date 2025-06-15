a8ede638e7ed1c6266d70d0f8894acb0a70dd0d0
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-14 20:34:48 -0400

    Add font-awesome-icons.ts file with initial icon definitions

---
+++ apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,16 @@
+import { IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+
+export const fontAwesomeIcons: IconDefinition[] = [
+  faUser,
+//   faCartShopping,
+//   faTruckFast,
+//   faYoutube,
+//   faFacebook,
+//   faTwitter,
+//   faXmark,
+//   faCheck,
+//   faShoppingCart,
+//   faCircleCheck,
+];
