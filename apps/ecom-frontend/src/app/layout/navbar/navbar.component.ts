08e98579f5f7434c26f778d6dce4c4aeed073fbd
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-16 18:58:49 -0400

    Add FontAwesomeModule to NavbarComponent imports

--- apps/ecom-frontend/src/app/layout/navbar/navbar.component.ts
+++ apps/ecom-frontend/src/app/layout/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'ecom-navbar',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
