9bc4a4e7af79b29f10a14fbda1be7ed1c6e17123
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-21 20:25:05 -0400

    Implement KindeJwtAuthenticationConverter to convert Jwt to AbstractAuthenticationToken

--- apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/shared/authentication/infrastructure/primary/KindeJwtAuthenticationConverter.java
+++ apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/shared/authentication/infrastructure/primary/KindeJwtAuthenticationConverter.java
@@ -1,4 +1,16 @@
 package io.github.ironslayer.ecom.shared.authentication.infrastructure.primary;
 
-public class KindeJwtAuthenticationConverter {
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+public class KindeJwtAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+  @Override
+  public AbstractAuthenticationToken convert(@NonNull Jwt source) {
+    return new JwtAuthenticationToken( source );
+  }
+
 }
