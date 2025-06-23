808342a192db2b241b3d453fac3004ba3fad8459
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-23 19:36:52 -0400

    Add CorsProperties class for CORS configuration

---
+++ apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/wire/security/primary/CorsProperties.java
@@ -0,0 +1,17 @@
+package io.github.ironslayer.ecom.wire.security.primary;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+public class CorsProperties {
+
+  @Bean
+  @ConfigurationProperties( prefix = "application.cors", ignoreInvalidFields = false)
+  public CorsConfiguration corsConfiguration() {
+    return new CorsConfiguration();
+  }
+
+}
