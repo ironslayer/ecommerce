91659a430d5facf54cdb8fb094f7236b7ce0a289
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-23 19:36:43 -0400

    Fix typo in SecurityConfiguration for authorizeHttpRequests method

--- apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/shared/authentication/infrastructure/primary/SecurityConfiguration.java
+++ apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/shared/authentication/infrastructure/primary/SecurityConfiguration.java
@@ -16,8 +16,8 @@ public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
     http.
-      authorizeHttpRequests( authtorize ->
-      authtorize
+      authorizeHttpRequests( authorize ->
+      authorize
         .requestMatchers( "/api/**" ).authenticated())
       .csrf( AbstractHttpConfigurer::disable )
       .oauth2ResourceServer( oauth2 -> oauth2.jwt( jwt -> jwt.jwtAuthenticationConverter( new KindeJwtAuthenticationConverter() ) ) );
