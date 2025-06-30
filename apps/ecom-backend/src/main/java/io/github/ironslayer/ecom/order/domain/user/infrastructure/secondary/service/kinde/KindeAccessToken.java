89b25845d1ee6700e1761eceb5f0feac4ad5cb60
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-06-29 20:21:04 -0400

    Remove UserEntity, JpaUserRepository, SpringDataUserRepository, KindeAccessToken, and KindeService classes

--- apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/order/domain/user/infrastructure/secondary/service/kinde/KindeAccessToken.java
+++
@@ -1,7 +0,0 @@
-package io.github.ironslayer.ecom.order.domain.user.infrastructure.secondary.service.kinde;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record KindeAccessToken(@JsonProperty("access_token") String accessToken,
-                               @JsonProperty("token_type") String tokenType) {
-}
