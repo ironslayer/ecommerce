dfa0baaab15a975e77b849254c3a524edf0326f7
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-03 21:36:43 -0400

    Add production environment configuration for Kinde authentication and API settings

---
+++ apps/ecom-frontend/src/environments/environment.production.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  kinde: {
+    authority: 'https://ironslayer.kinde.com',
+    redirectUrl: 'http://localhost:4200',
+    postLogoutRedirectUri: 'http://localhost:4200',
+    clientId: '4d130edd3f404fa88668903daabe6792',
+    audience: 'https://ironslayer.kinde.com/api',
+  },
+  apiUrl: 'http://localhost:8080/api',
+  stripePublishableKey: 'pk_test_51PqP6gFxB2dnnKdDhiB1cqZyvA0X1Si7ggeltnoO0OxXVRVMr35Lk8Z1UpsgbzjyawMNIpGlaB91S72uhbGvPpyJ00H5Zy8La7'
+
+};
