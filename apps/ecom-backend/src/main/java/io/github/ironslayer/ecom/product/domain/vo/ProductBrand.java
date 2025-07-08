c6ba710c0a024ad66020f0f29f1ee84c985c36e6
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-08 00:06:36 -0400

    Add ProductBrand value object with validation for non-null and minimum length

---
+++ apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/product/domain/vo/ProductBrand.java
@@ -0,0 +1,11 @@
+package io.github.ironslayer.ecom.product.domain.vo;
+
+
+import io.github.ironslayer.ecom.shared.error.domain.Assert;
+
+public record ProductBrand(String value) {
+
+  public ProductBrand {
+    Assert.field("value", value).notNull().minLength(3);
+  }
+}
