8dd97101c61a7526efef1ae1448defc45ff7d38a
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-08 00:06:18 -0400

    Add ProductRepository interface for product management operations

---
+++ apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/product/domain/repository/ProductRepository.java
@@ -0,0 +1,32 @@
+package io.github.ironslayer.ecom.product.domain.repository;
+
+import io.github.ironslayer.ecom.order.domain.order.vo.ProductPublicId;
+import io.github.ironslayer.ecom.product.domain.aggregate.FilterQuery;
+import io.github.ironslayer.ecom.product.domain.aggregate.Product;
+import io.github.ironslayer.ecom.product.domain.vo.PublicId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+
+  Product save(Product productToCreate);
+
+  Page<Product> findAll(Pageable pageable);
+
+  int delete(PublicId publicId);
+
+  Page<Product> findAllFeaturedProduct(Pageable pageable);
+
+  Optional<Product> findOne(PublicId publicId);
+
+  Page<Product> findByCategoryExcludingOne(Pageable pageable, PublicId categoryPublicId, PublicId productPublicId);
+
+  Page<Product> findByCategoryAndSize(Pageable pageable, FilterQuery filterQuery);
+
+  List<Product> findByPublicIds(List<PublicId> publicIds);
+
+  void updateQuantity(ProductPublicId productPublicId, long quantity);
+}
