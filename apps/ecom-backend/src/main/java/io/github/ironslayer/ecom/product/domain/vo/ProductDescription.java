197aa974e6cf6b2e7166f5016252319a755f70e6
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-08 00:06:42 -0400

    Add ProductDescription value object with validation for non-null and minimum length

---
+++ apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/product/domain/vo/ProductDescription.java
@@ -0,0 +1,11 @@
+package io.github.ironslayer.ecom.product.domain.vo;
+
+
+import io.github.ironslayer.ecom.shared.error.domain.Assert;
+
+public record ProductDescription(String value) {
+
+  public ProductDescription {
+    Assert.field("value", value).notNull().minLength(10);
+  }
+}
