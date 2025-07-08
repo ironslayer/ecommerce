4fbdbfcf29a77ed18376ca8363fb48c6316b70b1
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-08 00:05:10 -0400

    Add DetailCartResponse class for encapsulating cart response data

---
+++ apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/order/domain/order/aggregate/DetailCartResponse.java
@@ -0,0 +1,24 @@
+package io.github.ironslayer.ecom.order.domain.order.aggregate;
+
+import io.github.ironslayer.ecom.product.domain.aggregate.ProductCart;
+import org.jilt.Builder;
+
+import java.util.List;
+
+@Builder
+public class DetailCartResponse {
+
+  List<ProductCart> products;
+
+  public DetailCartResponse(List<ProductCart> products) {
+    this.products = products;
+  }
+
+  public List<ProductCart> getProducts() {
+    return products;
+  }
+
+  public void setProducts(List<ProductCart> products) {
+    this.products = products;
+  }
+}
