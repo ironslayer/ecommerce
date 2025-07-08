87b68eff26a47e5beafefc89e389d5678f434ad0
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-07 23:06:01 -0400

    Update environment configuration to use local audience URL

--- apps/ecom-frontend/src/environments/environment.ts
+++ apps/ecom-frontend/src/environments/environment.ts
@@ -4,7 +4,8 @@ export const environment = {
     redirectUrl: 'http://localhost:4200',
     postLogoutRedirectUri: 'http://localhost:4200',
     clientId: '4d130edd3f404fa88668903daabe6792',
-    audience: 'https://ironslayer.kinde.com/api',
+    // audience: 'https://ironslayer.kinde.com/api',
+    audience: 'http://localhost:8080/api',
   },
   apiUrl: 'http://localhost:8080/api',
   stripePublishableKey: 'pk_test_51PqP6gFxB2dnnKdDhiB1cqZyvA0X1Si7ggeltnoO0OxXVRVMr35Lk8Z1UpsgbzjyawMNIpGlaB91S72uhbGvPpyJ00H5Zy8La7'
