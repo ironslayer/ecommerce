f663892933f58e44441218b170cdfc9b22e87654
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-08 00:07:31 -0400

    Add JpaProductPictureRepository interface for picture data access

---
+++ apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/product/infrastructure/secondary/repository/JpaProductPictureRepository.java
@@ -0,0 +1,7 @@
+package io.github.ironslayer.ecom.product.infrastructure.secondary.repository;
+
+import fr.codecake.ecom.product.infrastructure.secondary.entity.PictureEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaProductPictureRepository extends JpaRepository<PictureEntity, Long> {
+}
