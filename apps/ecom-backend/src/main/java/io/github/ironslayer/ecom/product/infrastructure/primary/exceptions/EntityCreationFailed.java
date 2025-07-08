7169590ded1bf750f2cdc5108a2eff3677800cfb
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-08 00:07:14 -0400

    Add EntityCreationFailed exception for handling entity creation errors

---
+++ apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/product/infrastructure/primary/exceptions/EntityCreationFailed.java
@@ -0,0 +1,7 @@
+package io.github.ironslayer.ecom.product.infrastructure.primary.exceptions;
+
+public class EntityCreationFailed extends RuntimeException {
+  public EntityCreationFailed(String message) {
+    super(message);
+  }
+}
