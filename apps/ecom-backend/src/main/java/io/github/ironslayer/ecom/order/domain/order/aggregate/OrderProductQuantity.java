f6614dc00af0730f0bff9cde941bda648d4e066a
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-08 00:05:20 -0400

    Add OrderProductQuantity record for managing product quantities in orders

---
+++ apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/order/domain/order/aggregate/OrderProductQuantity.java
@@ -0,0 +1,9 @@
+package io.github.ironslayer.ecom.order.domain.order.aggregate;
+
+import io.github.ironslayer.ecom.order.domain.order.vo.OrderQuantity;
+import io.github.ironslayer.ecom.order.domain.order.vo.ProductPublicId;
+import org.jilt.Builder;
+
+@Builder
+public record OrderProductQuantity(OrderQuantity quantity, ProductPublicId productPublicId) {
+}
