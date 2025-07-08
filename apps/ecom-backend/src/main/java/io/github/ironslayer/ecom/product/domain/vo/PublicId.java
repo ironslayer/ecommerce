ffe7a31ba10d42f68a300bf0c3b689dc7e9a7cc8
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-08 00:06:52 -0400

    Add PublicId value object with validation for non-null UUID

---
+++ apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/product/domain/vo/PublicId.java
@@ -0,0 +1,13 @@
+package io.github.ironslayer.ecom.product.domain.vo;
+
+
+import io.github.ironslayer.ecom.shared.error.domain.Assert;
+
+import java.util.UUID;
+
+public record PublicId(UUID value) {
+
+  public PublicId {
+    Assert.notNull("value", value);
+  }
+}
