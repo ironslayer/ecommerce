51bfd06dbf199f7db0d1c8a68c519c729a978dd8
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-09 11:23:12 -0400

    Fix formatting in AuthorityName record declaration

--- apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/order/domain/user/vo/AuthorityName.java
+++ apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/order/domain/user/vo/AuthorityName.java
@@ -2,10 +2,9 @@ package io.github.ironslayer.ecom.order.domain.user.vo;
 
 import io.github.ironslayer.ecom.shared.error.domain.Assert;
 
-public record AuthorityName(String name ) {
+public record AuthorityName(String name) {
 
   public AuthorityName {
     Assert.field("name", name).notNull();
   }
-
 }
