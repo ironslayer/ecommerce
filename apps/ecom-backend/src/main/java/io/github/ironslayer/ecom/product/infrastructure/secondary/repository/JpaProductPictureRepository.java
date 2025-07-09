89174a972af3fd0c8c8ef875a4b93ae0e18eb341
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-09 11:26:08 -0400

    Fix import statement in JpaProductPictureRepository.java

--- apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/product/infrastructure/secondary/repository/JpaProductPictureRepository.java
+++ apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/product/infrastructure/secondary/repository/JpaProductPictureRepository.java
@@ -1,6 +1,6 @@
 package io.github.ironslayer.ecom.product.infrastructure.secondary.repository;
 
-import fr.codecake.ecom.product.infrastructure.secondary.entity.PictureEntity;
+import io.github.ironslayer.ecom.product.infrastructure.secondary.entity.PictureEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaProductPictureRepository extends JpaRepository<PictureEntity, Long> {
