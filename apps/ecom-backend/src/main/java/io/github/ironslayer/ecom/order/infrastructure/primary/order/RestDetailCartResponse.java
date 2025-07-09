1d652a92486c3920fe7dea6d209c707e9a71e617
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-09 11:23:35 -0400

    Add RestDetailCartResponse record to handle detailed cart responses

---
+++ apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/order/infrastructure/primary/order/RestDetailCartResponse.java
@@ -0,0 +1,16 @@
+package io.github.ironslayer.ecom.order.infrastructure.primary.order;
+
+import io.github.ironslayer.ecom.order.domain.order.aggregate.DetailCartResponse;
+import org.jilt.Builder;
+
+import java.util.List;
+
+@Builder
+public record RestDetailCartResponse(List<RestProductCart> products) {
+
+  public static RestDetailCartResponse from(DetailCartResponse detailCartResponse) {
+    return RestDetailCartResponseBuilder.restDetailCartResponse()
+      .products(detailCartResponse.getProducts().stream().map(RestProductCart::from).toList())
+      .build();
+  }
+}
