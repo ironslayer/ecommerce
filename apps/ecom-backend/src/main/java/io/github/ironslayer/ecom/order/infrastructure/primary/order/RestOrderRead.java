bfd86f79bcafe08936855bc2d39f0a2189ffa7c5
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-09 11:23:43 -0400

    Add RestOrderRead record to represent order details in API responses

---
+++ apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/order/infrastructure/primary/order/RestOrderRead.java
@@ -0,0 +1,22 @@
+package io.github.ironslayer.ecom.order.infrastructure.primary.order;
+
+import io.github.ironslayer.ecom.order.domain.order.aggregate.Order;
+import io.github.ironslayer.ecom.order.domain.order.vo.OrderStatus;
+import org.jilt.Builder;
+
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+public record RestOrderRead(UUID publicId,
+                            OrderStatus status,
+                            List<RestOrderedItemRead> orderedItems) {
+
+  public static RestOrderRead from(Order order) {
+    return RestOrderReadBuilder.restOrderRead()
+      .publicId(order.getPublicId().value())
+      .status(order.getStatus())
+      .orderedItems(RestOrderedItemRead.from(order.getOrderedProducts()))
+      .build();
+  }
+}
