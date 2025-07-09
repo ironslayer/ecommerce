61c0a598909526a069135291727df1bad12dd396
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-07-09 11:23:08 -0400

    Add missing import for UserEntity in UserReader.java

--- apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/order/domain/user/service/UserReader.java
+++ apps/ecom-backend/src/main/java/io/github/ironslayer/ecom/order/domain/user/service/UserReader.java
@@ -1,10 +1,10 @@
 package io.github.ironslayer.ecom.order.domain.user.service;
 
-
 import io.github.ironslayer.ecom.order.domain.user.aggregate.User;
 import io.github.ironslayer.ecom.order.domain.user.repository.UserRepository;
 import io.github.ironslayer.ecom.order.domain.user.vo.UserEmail;
 import io.github.ironslayer.ecom.order.domain.user.vo.UserPublicId;
+import io.github.ironslayer.ecom.order.infrastructure.secondary.entity.UserEntity;
 
 import java.util.Optional;
 
